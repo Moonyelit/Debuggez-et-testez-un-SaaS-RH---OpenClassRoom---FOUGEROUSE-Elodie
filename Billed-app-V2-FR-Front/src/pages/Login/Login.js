8cf669e97e14f0ee1173c2be69c4fcd24b6bd76c
Moonyelit <fougerouse.elodie@gmail.com>
2026-05-16 21:15:08 +0200

    🐛 fix: corriger les sélecteurs du formulaire admin

--- Billed-app-V2-FR-Front/src/pages/Login/Login.js
+++ Billed-app-V2-FR-Front/src/pages/Login/Login.js
@@ -46,8 +46,8 @@ const handleSubmitAdmin = (e, { localStorage, onNavigate, store }) => {
 
   const user = {
     type: "Admin",
-    email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-    password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+    email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+    password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
     status: "connected"
   }
 
